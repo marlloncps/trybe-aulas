2f7138a688d4a5fcdeebb3155f10f2a60d38b0ef
Marllon Cesar <marllon.cps@gmail.com>
2021-10-14 16:21:35 -0300

    Adicionando algumas aulas que já tinha no meu pc

---
+++ aula4.1/script.js
@@ -0,0 +1,15 @@
+let weekDay = 8;
+
+switch (weekDay) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+    case 7:
+        console.log("Dia de aprendizado =)")
+        break;
+    default:
+        console.log("Não existe esse dia ainda :)")
+}
