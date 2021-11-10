bd3ed6e78819150b71900f68a1e44b9e28254356
Marllon Cesar <marllon.cps@gmail.com>
2021-11-10 18:26:37 -0300

    Fim da aula 7.1

---
+++ aula-7.1/script.js
@@ -0,0 +1,19 @@
+const to = 'Marllon';
+const value = '22';
+
+const email = `Olá ${to},
+O valor da sua cobrança é de ${value}
+Esta é uma mensagem automática.`;
+console.log(email);
+
+
+// 
+
+const teste = 50;
+
+function tesste () {
+    const teste = 49;
+    return teste;
+}
+console.log(teste);
+console.log(tesste());
