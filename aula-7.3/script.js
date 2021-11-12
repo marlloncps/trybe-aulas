8f0a15f6de3d464abf40d3bf6051427d8a9e26d0
Marllon Cesar <marllon.cps@gmail.com>
2021-11-12 17:50:46 -0300

    Implementa a funcao summationOf para qualquer cenário

--- aula-7.3/script.js
+++ aula-7.3/script.js
@@ -1,3 +1,11 @@
 const sum = (a, b) => a + b;
 
-module.exports = sum;
+module.exports = { sum, summationOf };
+
+function summationOf(value) {
+  let total = 0;
+  for (let i = 1; i <= value; i++) {
+    total = total + i;
+  }
+  return total;
+}
--- aula-7.3/script.spec.js
+++ aula-7.3/script.spec.js
@@ -1,4 +1,4 @@
-const sum = require("./script");
+const { sum, summationOf } = require("./script");
 
 test("Soma de dois valores", () => {
   expect(sum(2, 3)).toBe(5);
@@ -6,4 +6,22 @@ test("Soma de dois valores", () => {
 
 test("Arredondamento", () => {
   expect(sum(5.1, 6.55)).toBeCloseTo(11.65);
+});
+
+// TDD
+
+test("Verificar se a função summationOf existe.", () => {
+  expect(typeof summationOf).toBe("function");
+});
+
+test("Verificar se a função summationOf retorna 1 para o parametro 1.", () => {
+  expect(summationOf(1)).toBe(1);
+});
+
+test("Verificar se a função summationOf retorna 6 para o parametro 3.", () => {
+  expect(summationOf(3)).toBe(6);
+});
+
+test("Verificar se a função summationOf retorna 15 para o parametro 5.", () => {
+  expect(summationOf(5)).toBe(15);
 });
