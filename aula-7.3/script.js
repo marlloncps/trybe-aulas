03604ea285bff5a8afbeb210d5f95d86016093f4
Marllon Cesar <marllon.cps@gmail.com>
2021-11-12 18:40:56 -0300

    Adicionando arquivos finais da aula 7.3

--- aula-7.3/script.js
+++ aula-7.3/script.js
@@ -3,9 +3,16 @@ const sum = (a, b) => a + b;
 module.exports = { sum, summationOf };
 
 function summationOf(value) {
+  if (value === '') {
+    throw new Error("Precisa de um valor para continuar a função");
+  }
+//   if(!Number.isNaN(value)){
+//     throw new Error("Apenas números são permitidos. MACACO!"); 
+//   }
   let total = 0;
   for (let i = 1; i <= value; i++) {
     total = total + i;
   }
   return total;
 }
+console.log(summationOf("."));
--- aula-7.3/script.spec.js
+++ aula-7.3/script.spec.js
@@ -1,27 +1,32 @@
 const { sum, summationOf } = require("./script");
 
-test("Soma de dois valores", () => {
-  expect(sum(2, 3)).toBe(5);
-});
+describe("Teste relativos a funcao sum", () => {
+  test("Soma de dois valores", () => {
+    expect(sum(2, 3)).toBe(5);
+  });
 
-test("Arredondamento", () => {
-  expect(sum(5.1, 6.55)).toBeCloseTo(11.65);
+  test("Arredondamento", () => {
+    expect(sum(5.1, 6.55)).toBeCloseTo(11.65);
+  });
 });
-
 // TDD
+describe("Teste relativos a funcao summationOf", () => {
+  test("Verificar se a função summationOf existe.", () => {
+    expect(typeof summationOf).toBe("function");
+  });
 
-test("Verificar se a função summationOf existe.", () => {
-  expect(typeof summationOf).toBe("function");
-});
+  test("Verificar se a função summationOf retorna 1 para o parametro 1.", () => {
+    expect(summationOf(1)).toBe(1);
+  });
 
-test("Verificar se a função summationOf retorna 1 para o parametro 1.", () => {
-  expect(summationOf(1)).toBe(1);
-});
+  test("Verificar se a função summationOf retorna 6 para o parametro 3.", () => {
+    expect(summationOf(3)).toBe(6);
+  });
 
-test("Verificar se a função summationOf retorna 6 para o parametro 3.", () => {
-  expect(summationOf(3)).toBe(6);
-});
-
-test("Verificar se a função summationOf retorna 15 para o parametro 5.", () => {
-  expect(summationOf(5)).toBe(15);
+  test("Verificar se a função summationOf retorna 15 para o parametro 5.", () => {
+    expect(summationOf(5)).toBe(15);
+  });
+  test("Verificar se a função summationOf retorna Precisa de um valor para continuar a função para o parametro vazio.", () => {
+    expect(() => {summationOf('')}).toThrow('Precisa de um valor para continuar a função');
+  });
 });
